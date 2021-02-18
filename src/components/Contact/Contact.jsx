7d663051e0af65e448603dcad49cffc1498e62ca
Jordan Utz <jordanutz@outlook.com>
2021-02-18 16:48:20 -0500

    Added font-face for Codex Pro and dredd

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = ({contact}) => {
                   <Col xs={24}>
                      <section className="contact__overview">
                            <h2>Contact</h2>
-                           <p>I <span role="img" aria-label="love">💜</span> my job, so I'm not currently seeking any employment opportunities at this time, but if you'd like to discuss a freelance opportunity please drop me a line and I will do my best to answer your email as soon as I can!</p>
+                           <p>I <span role="img" aria-label="love">💜</span> my job, so I'm currently not seeking any employment opportunities at this time, but if you'd like to discuss a freelance opportunity please drop me a line and I will do my best to answer your email as soon as I can!</p>
                            <a href="mailto:jordanutz@outlook.com" target="_blank" rel="noopener noreferrer">
                               <button>Drop a Line</button>
                            </a>
